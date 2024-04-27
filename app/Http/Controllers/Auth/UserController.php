21faf2d1fd49cf52707e56b045d5f37153593609
mahdipaykoub <mahdi.paykoub.github@gmail.com>
2024-04-27 16:49:48 +0330

    add laravel sanctum to project

---
+++ app/Http/Controllers/Auth/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+
+
+class UserController extends Controller
+{
+    public function register()
+    {
+        
+        
+    }
+    public function login()
+    {
+
+    }
+
+    public function logout()
+    {
+        
+    }
+
+}
--- routes/admin/api.php
+++ routes/admin/api.php
@@ -2,9 +2,12 @@
 
 use App\Http\Controllers\Admin\ArticleController;
 use App\Http\Controllers\Admin\CategoryController;
+
 use Illuminate\Support\Facades\Route;
 
 
 Route::resource('article', ArticleController::class);
 
 Route::resource('category', CategoryController::class);
+
+
